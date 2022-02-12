edf35b1269cf590094ac0e00ffbe36a82f662b75
Koissi Adjorlolo <yotcho2@gmail.com>
2022-02-12 16:56:55 -0500

    made contains shorter

--- src/lambda_pi/contains.ts
+++ src/lambda_pi/contains.ts
@@ -1,11 +1,6 @@
-import { Application, Ast, Lambda, Pi } from "./ast";
+import { Ast } from "./ast";
+import { children_of } from "./children_of";
 import { syntactic_equality } from "./syntactic_equality";
 
-export function contains(parent: Ast, child: Ast): boolean {
-    const children_of = (ast: Ast): Ast[] =>
-        ast instanceof Application ? [ast.head, ast.arg]
-        : ast instanceof Lambda ? [ast.type, ast.scope]
-        : ast instanceof Pi ? [ast.type, ast.scope]
-        : []
-    return children_of(parent).some((c) => syntactic_equality(child, c) || contains(c, child))
-}
+export const contains = (parent: Ast, child: Ast): boolean =>
+    children_of(parent).some((c) => syntactic_equality(child, c) || contains(c, child))
